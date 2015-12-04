972ff03eeeeddf8117d1bd6d87aa9dab115b72fe
Bill Glover <bill@billglover.co.uk>
2015-12-04 22:33:37 +0000

    Calculate date offset

--- pinfetcher.go
+++ pinfetcher.go
@@ -2,11 +2,24 @@ package main
 
 import "flag"
 import "fmt"
+import "time"
 
 func main() {
+	// --api-key
 	apiKeyPtr := flag.String("api-key", "", "your PinBoard API key")
-
+	daysOffsetPtr := flag.Int("d", 7, "number of days to retrieve")
 	flag.Parse()
 
-	fmt.Println("api-key:", *apiKeyPtr)
+	// time
+	currentTime := time.Now().Local()
+	yesterdayTime := currentTime.AddDate(0, 0, -1)
+	lastWeekTime := currentTime.AddDate(0, 0, -1 * *daysOffsetPtr -1)
+	yesterday := yesterdayTime.Round(time.Hour*24).Format(time.RFC3339)
+	lastWeek := lastWeekTime.Round(time.Hour*24).Format(time.RFC3339)
+	fmt.Println("From: ", lastWeek)
+	fmt.Println("To: ", yesterday)
+
+	// print command line arguments
+	fmt.Println("--api-key", *apiKeyPtr)
+	fmt.Println("-d", *daysOffsetPtr)
 }	
